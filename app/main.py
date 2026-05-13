1589c7cd80a8902723b57d80b679277ae0e3f83a
Emilio Francisco Vazquez Perez <emilioVPSis@gmail.com>
2026-05-12 21:02:15 -0600

    Cambios para despliegue

--- app/main.py
+++ app/main.py
@@ -4,19 +4,23 @@ from app.api import auth, survey, scan, diet, streaks
 
 app = FastAPI(title="NutriFit AI API")
 
+origins = [
+    "https://practica-open-api.vercel.app",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], # Permitir frontend en desarrollo
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-app.include_router(auth.router, prefix="/api/auth",      tags=["auth"])
-app.include_router(survey.router, prefix="/api/survey",  tags=["survey"])
+app.include_router(auth.router, prefix="/api/auth", tags=["auth"])
+app.include_router(survey.router, prefix="/api/survey", tags=["survey"])
 app.include_router(scan.router, prefix="/api/scan-food", tags=["scan"])
-app.include_router(diet.router, prefix="/api/diet",      tags=["diet"])
-app.include_router(streaks.router, prefix="/api/streaks",  tags=["streaks"])
+app.include_router(diet.router, prefix="/api/diet", tags=["diet"])
+app.include_router(streaks.router, prefix="/api/streaks", tags=["streaks"])
 
 
 @app.get("/")
